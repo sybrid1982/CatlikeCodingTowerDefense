f14e2b2307f42de48ba03c47694292151b361ce8
Sydney Bridges <sbridges@nexient.com>
2019-09-16 07:35:17 -0400

    New master branch to replace old one with conflicts (hopefully)

---
+++ Assets/Scripts/SpawnOrder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOrder
+{
+    float _time, _spawnRate;
+    public float time { get => _time; }
+    public float spawnRate { get => _spawnRate; }
+
+    public SpawnOrder(float time, float spawnRate)
+    {
+        _time = time;
+        _spawnRate = spawnRate;
+    }
+}
---
+++ Assets/Scripts/SpawnTimeline.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SpawnTimeline : ScriptableObject
+{
+    public SpawnOrder[] spawnOrders;
+    int currentIndexPos;
+    public SpawnOrder GetSpawnOrder(float time)
+    {
+        if(spawnOrders[currentIndexPos+1].time > time)
+        {
+            currentIndexPos++;
+            return spawnOrders[currentIndexPos];
+        } else
+        {
+            return new SpawnOrder(0, -1);
+        }
+    }
+}
